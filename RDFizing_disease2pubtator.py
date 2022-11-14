634adb63481c37e4760a5eb55e11076bdd114b95
yayamamo <yayamamo+github@gmail.com>
2022-11-15 00:56:10 +0900

    Changed to a filter program.

--- RDFizing_disease2pubtator.py
+++ RDFizing_disease2pubtator.py
@@ -4,26 +4,23 @@ import sys
 import json
 import re
 import codecs
-import csv
 from rdflib import Namespace, URIRef, Graph, BNode, Literal
 from rdflib.namespace import RDF, RDFS, FOAF
 
-csv.field_size_limit(1000000000)
-
-
 # make rdf file
-def make_rdf(infile_pubtator, outfile_rdf):
-    g = Graph()
 
-    data        = Namespace("http://www.w3.org/ns/oa#")
-    ns_oa       = Namespace("http://www.w3.org/ns/oa#")
-    ns_dcterms  = Namespace("http://purl.org/dc/terms/")
-    ns_pubmed   = Namespace("http://rdf.ncbi.nlm.nih.gov/pubmed/")
-    ns_dbsnp    = Namespace("http://identifiers.org/dbsnp/")
-    ns_ncbigene = Namespace("http://identifiers.org/ncbigene/")
-    ns_mesh     = Namespace("http://identifiers.org/mesh/")
-    ns_omim     = Namespace("http://identifiers.org/omim/")
-    
+ns_oa       = Namespace("http://www.w3.org/ns/oa#")
+ns_dcterms  = Namespace("http://purl.org/dc/terms/")
+ns_pubmed   = Namespace("http://rdf.ncbi.nlm.nih.gov/pubmed/")
+ns_dbsnp    = Namespace("http://identifiers.org/dbsnp/")
+ns_ncbigene = Namespace("http://identifiers.org/ncbigene/")
+ns_mesh     = Namespace("http://identifiers.org/mesh/")
+ns_omim     = Namespace("http://identifiers.org/omim/")
+ns_subj     = Namespace("http://purl.jp/bio/10/pubtator-central/Disease/")
+
+def init_graph():
+
+    g = Graph()
     g.bind('oa', ns_oa)
     g.bind('dcterms', ns_dcterms)
     g.bind('pubmed', ns_pubmed)
@@ -31,15 +28,28 @@ def make_rdf(infile_pubtator, outfile_rdf):
     g.bind('ncbigene', ns_ncbigene)
     g.bind('mesh', ns_mesh)
     g.bind('omim', ns_omim)
+    g.bind('pc', ns_subj)
 
+    return(g)
 
-    fh_in = open(infile_pubtator, 'r')
-    #reader = csv.reader(fh_in, delimiter="\t")
-    lines = fh_in.readlines()
-    for line in lines:
+def make_rdf(start_number):
+
+    row_num = start_number
+    step = 10000
+    count = 0
+
+    for line in sys.stdin:
+        if(count == 0):
+            g = init_graph()
+        elif(count == step):
+            count = 0
+            sys.stdout.write(g.serialize(format='ntriples').decode('ascii'))
+            g = init_graph()
+
         row = line.rstrip('\n').split('\t')
         try:
             pmid     = row[0]
+            rtype    = row[1]
             disease  = row[2]
             mention  = row[3]
             resource = row[4]
@@ -51,10 +61,10 @@ def make_rdf(infile_pubtator, outfile_rdf):
         if pmid == "PMID":
             continue
 
-        blank = BNode()
-
-        g.add( (blank, RDF.type, URIRef(ns_oa.Annotation)) )
-        g.add( (blank, URIRef(ns_oa.hasTarget), URIRef(ns_pubmed + pmid)) )
+        subject = URIRef(ns_subj + str(row_num))
+        g.add( (subject, RDF.type, URIRef(ns_oa.Annotation)) )
+        g.add( (subject, URIRef(ns_dcterms + 'subject'), Literal(rtype)) )
+        g.add( (subject, URIRef(ns_oa.hasTarget), URIRef(ns_pubmed + pmid)) )
 
         # add disease id triple
         match_mesh = re.match(r'^MESH', disease)
@@ -63,29 +73,31 @@ def make_rdf(infile_pubtator, outfile_rdf):
             mesh = disease.replace('MESH:', '')
             list_mesh = mesh.split('|')
             for m in list_mesh:
-                g.add( (blank, URIRef(ns_oa.hasBody), URIRef(ns_mesh + m)) )
+                g.add( (subject, URIRef(ns_oa.hasBody), URIRef(ns_mesh + m)) )
         elif match_omim:
             omim = disease.replace('OMIM:', '')
             list_omim = omim.split('|')
             for o in list_omim:
-                g.add( (blank, URIRef(ns_oa.hasBody), URIRef(ns_omim + o)) )
+                g.add( (subject, URIRef(ns_oa.hasBody), URIRef(ns_omim + o)) )
 
         # add resource triple
         for s in list_resource:
-            g.add( (blank, URIRef(ns_dcterms.source), Literal(s)) )
+            g.add( (subject, URIRef(ns_dcterms.source), Literal(s)) )
     
     # output RDF
-    g.serialize(destination=outfile_rdf, format='turtle')
+        row_num = row_num + 1
+        count = count + 1
 
-    fh_in.close()
+    if(count > 0):
+        sys.stdout.write(g.serialize(format='ntriples').decode('ascii'))
+
     return
-
 
 # main
 if __name__ == "__main__":
     params = sys.argv
-    infile_pubtator  = params[1]
-    outfile_rdf      = params[2]
-    make_rdf(infile_pubtator, outfile_rdf)
-
-
+    if len(params) == 2:
+        start_number = params[1]
+    else:
+        start_number = 0
+    make_rdf(start_number)
